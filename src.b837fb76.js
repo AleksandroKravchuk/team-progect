1d4c2a795bb040b38b9f4c536d828557ad963914
AleksandroKravchuk <93790774+AleksandroKravchuk@users.noreply.github.com>
2022-01-26 20:38:04 +0000

    Deploying to gh-pages from @ AleksandroKravchuk/team-progect@e9ca330496e7bd1716546f6ad5affd0c44c421dd 🚀

--- src.b837fb76.js
+++ src.b837fb76.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-progect/src.61f87c08.js.map
+//# sourceMappingURL=/team-progect/src.b837fb76.js.map
