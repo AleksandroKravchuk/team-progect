b38c05834a6e6fae772498813b8c2c962cae938b
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-01-29 21:54:34 +0200

    добавляю скролл

---
+++ src/js/scroll.js
@@ -0,0 +1,9 @@
+$('a[href^="#"').on('click', function () {
+
+        let href = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        });
+        return false;
+    });
