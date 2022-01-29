6ec52c52ee610d62a8df6037996ff52ded8f163e
AleksandroKravchuk <93790774+AleksandroKravchuk@users.noreply.github.com>
2022-01-29 08:49:54 +0000

    Deploying to gh-pages from @ AleksandroKravchuk/team-progect@5360792a420893617c1aaa9ab735f6d6e2e76a5f 🚀

--- src.2c1ebe26.js
+++ src.2c1ebe26.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/gallery-section/gallery-bck.jpg":[["gallery-bck.81ee89cb.jpg","rUQ1"],"rUQ1"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-progect/src.caf42497.js.map
+//# sourceMappingURL=/team-progect/src.2c1ebe26.js.map
