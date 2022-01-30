4f48d92d7ffe280c94f285d6ff846fcc71b47ad8
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-01-30 17:12:35 +0200

    1

---
+++ src/js/probnik.js
@@ -0,0 +1,20 @@
+// $(document).ready(function () {
+//            $('.autor__list-main').slick({
+//             arrow: false,
+//             fade: true,
+//             asNavFor:".autor__list"
+//         });
+
+//         $('.autor__list').slick({
+//             infinite: true,
+//             slidesToShow: 2,
+//             slidesToScroll: 1,
+//             centerMode: true,
+//             variableWidth: true,
+//             asNavFor:".autor__list-main"
+//         });
+//         $('.single-item-rtl').slick({
+//             rtl: true
+//         });
+       
+//     });
--- src/js/scroll.js
+++
@@ -1,9 +0,0 @@
-$('a[href^="#"').on('click', function () {
-
-        let href = $(this).attr('href');
-
-        $('html, body').animate({
-            scrollTop: $(href).offset().top
-        });
-        return false;
-    });
