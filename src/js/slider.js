bfeb8750dae901dfdfc4e9bdc5f0faf4b6e59dae
Svitlana-Lukachyk <svitlanalukachyk@gmail.com>
2022-01-31 05:00:29 +0200

    Додає стилі до слайдера

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,7 +1,13 @@
-$('.reviews__list').slick({
+$(document).ready(function () {
+  $('.reviews__list').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
   });
+});
