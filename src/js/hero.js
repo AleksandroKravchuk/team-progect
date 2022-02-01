1e1e0e9153db06fca6e2019560ac2a8a0263f415
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-02-01 18:12:00 +0200

    1

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,15 +1,15 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open-main]'),
-    closeModalBtn: document.querySelector('[data-modal-close-main]'),
-    modal: document.querySelector('[data-modal-main]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open-main]'),
+//     closeModalBtn: document.querySelector('[data-modal-close-main]'),
+//     modal: document.querySelector('[data-modal-main]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    document.body.classList.toggle('modal-open-main');
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
+//   function toggleModal() {
+//     document.body.classList.toggle('modal-open-main');
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
